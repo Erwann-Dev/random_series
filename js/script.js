c86624c0f2ccac0be16811d76e402df1034b84bd
Erwann-Dev <erwann.dev@gmail.com>
2021-04-05 18:27:03 +0200

    A big change

--- js/script.js
+++ js/script.js
@@ -254,6 +254,79 @@ function sg_1_random(){
   SG.innerHTML = ("SG-1 : Saison : "+season_result+" Episode : "+episode_result+" Titre : "+title)
 }
 
+function custom_random(){
+  var key = "3a1dd21b1318e5d4bd73b5b9b3bd7e74";
+  custom = document.getElementById("custom_id");
+	var id = custom.value;
+  if(custom.value == ""){
+		custom.value = prompt("Tu a oublier d'indiquer un id indique la ici : ");
+    id = custom.value;
+	}
+  reset();
+  var title_global = "";
+  function season_random(){
+    var settings = {
+      async: false,
+      "url": "https://api.themoviedb.org/3/tv/"+id+"?api_key="+key,
+      "method": "GET"
+    }
+    
+    $.ajax(settings).done(function (response) {
+      title_global = response.name;
+      console.log(settings.url);
+      season_alea = (Math.floor(Math.random() * response.seasons.length)); 
+      console.log("Saison : "+season_alea);
+      console.log(response.seasons);
+    });
+    return season_alea;
+  }
+
+  function episode_random(season_alea){
+    var settings_2 = {
+      async: false,
+      "url": "https://api.themoviedb.org/3/tv/"+id+"/season/"+season_alea+"?api_key="+key,
+      "method": "GET"
+    }
+    $.ajax(settings_2).done(function (response) {
+      console.log(settings_2.url)
+      episode_alea = (Math.floor(Math.random() * response.episodes.length)+1); 
+      console.log("Episode : "+episode_alea);
+      console.log(response.episodes);
+    });
+    return episode_alea;
+  }
+
+  function result(season_alea,episode_alea){
+    var settings_3 = {
+      async: false,
+      "url": "https://api.themoviedb.org/3/tv/"+id+"/season/"+season_alea+"/episode/"+episode_alea+"?api_key="+key,
+      "method": "GET"
+    }
+
+    $.ajax(settings_3).done(function (response) {
+      console.log(settings_3.url)
+      console.log(response.name);
+      resultat = response.name
+    });
+    return resultat;
+  }
+
+  var season_alea;
+  var season_result = season_random();
+  console.log("var season : "+season_result);
+  while (season_result == 0){
+    season_result = season_random();
+  }
+  console.log("var season : "+season_result);
+  var episode_result = episode_random(season_result);
+  console.log("var episode : "+episode_result);
+  var title = result(season_result,episode_result)
+  serie_title = document.querySelector(".Serie")
+  serie_title.innerHTML = ("Serie : "+title_global)
+  Custom = document.querySelector(".Custom")
+  Custom.innerHTML = (title_global+" : Saison : "+season_result+" Episode : "+episode_result+" Titre : "+title)
+}
+
 function generate(){
   serie = (Math.floor(Math.random() * 4))+1;
   psg = document.querySelector(".SG-1")
@@ -267,28 +340,28 @@ function generate(){
     psg.innerHTML = ""
     patlantis.innerHTML = ""
     puniverse.innerHTML = ""
-    pserie.innerHTML = ("Serie : Doctor Who")
+    pserie.innerHTML = ""
   }
   else if (serie == 2){
     universe_random()
     psg.innerHTML = ""
     patlantis.innerHTML = ""
     pdoctor.innerHTML = ""
-    pserie.innerHTML = ("Serie : Stagate Universe")
+    pserie.innerHTML = ""
   }
   else if (serie == 3){
     atlantis_random()
     psg.innerHTML = ""
     puniverse.innerHTML = ""
     pdoctor.innerHTML = ""
-    pserie.innerHTML = ("Serie : Stagate Atlantis")
+    pserie.innerHTML = ""
   }
   else if (serie == 4){
     sg_1_random()
     patlantis.innerHTML = ""
     puniverse.innerHTML = ""
     pdoctor.innerHTML = ""
-    pserie.innerHTML = ("Serie : Stagate SG-1")
+    pserie.innerHTML = ""
   }
   else if (serie == 5){
     window.alert("WTF 5 !!!!")
@@ -301,7 +374,9 @@ function reset(){
   puniverse = document.querySelector(".Universe")
   pdoctor = document.querySelector(".Doctor_Who")
   pserie = document.querySelector(".Serie")
+  pcustom = document.querySelector(".Custom")
 
+  pcustom.innerHTML = ""
   pserie.innerHTML = ""
   psg.innerHTML = ""
   patlantis.innerHTML = ""
